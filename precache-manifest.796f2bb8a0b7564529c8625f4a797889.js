53599ecd7987e024109638752b49b351c4007ef9
VirtuuX <somargame22@gmail.com>
2022-05-10 18:09:44 +0600

    Updates

---
+++ precache-manifest.796f2bb8a0b7564529c8625f4a797889.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50069301e353b6c8cd5f969ac36ae6a",
+    "url": "/UpCraft/index.html"
+  },
+  {
+    "revision": "bf9128e8351350d5a116",
+    "url": "/UpCraft/static/css/main.4720f303.chunk.css"
+  },
+  {
+    "revision": "4fe95309cb307958b6b8",
+    "url": "/UpCraft/static/js/2.6c8ca8e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/UpCraft/static/js/2.6c8ca8e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9128e8351350d5a116",
+    "url": "/UpCraft/static/js/main.ff1e3a9c.chunk.js"
+  },
+  {
+    "revision": "a84d58e7b6eb9f212fc2",
+    "url": "/UpCraft/static/js/runtime-main.e9375faa.js"
+  },
+  {
+    "revision": "7d1d4a0bec1227fee2a49011500752a7",
+    "url": "/UpCraft/static/media/UpClassic.7d1d4a0b.jpg"
+  },
+  {
+    "revision": "a58d72edf6fada1d536ee4ba56af6914",
+    "url": "/UpCraft/static/media/UpMagic.a58d72ed.jpg"
+  },
+  {
+    "revision": "b1cefca9aa7cd33f191e9269d2f39561",
+    "url": "/UpCraft/static/media/UpMagicRpg.b1cefca9.jpg"
+  },
+  {
+    "revision": "3eb882c4c7494d54ce41b21c6397bbb0",
+    "url": "/UpCraft/static/media/UpRpg.3eb882c4.jpg"
+  },
+  {
+    "revision": "8c20307fc42483bf4bff44f7a0c53dc9",
+    "url": "/UpCraft/static/media/background.8c20307f.jpg"
+  },
+  {
+    "revision": "a000e4fdaf21b7865fa5a661363b1e25",
+    "url": "/UpCraft/static/media/linux.a000e4fd.svg"
+  },
+  {
+    "revision": "99edd2f6f7d265f6fd19903a370db40c",
+    "url": "/UpCraft/static/media/windows.99edd2f6.svg"
+  }
+]);
